a47f71a27b42964ffe4008a35bbf1cd141cdbce9
Anders Urrang <anders.urrang@unimicro.no>
2023-11-23 00:29:43 +0100

    wip

--- src/Plugin.svelte
+++ src/Plugin.svelte
@@ -1,15 +1,27 @@
-<svelte:options customElement="unimicro-plugin" />
+<svelte:options
+    customElement={{
+        tag: 'unimicro-plugin',
+        props: {
+            api: { reflect: true, type: 'Object', attribute: 'api' },
+        },
+    }}
+/>
 
 <script lang="ts">
+    export let api: any; // TODO: types
+
     let count: number = 0;
-    const increment = () => {
+
+    function increment() {
         count += 1;
-    };
+    }
 </script>
 
 <button on:click={increment}>
     Count: {count}
 </button>
+
+<button on:click={api.showAlert('Hello from plugin!')}> Show alert </button>
 
 <style>
 </style>
