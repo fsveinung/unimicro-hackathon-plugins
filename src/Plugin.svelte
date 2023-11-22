9f6c499e3cdec79ff68cc64c5690cafe37999eac
Anders Urrang <anders.urrang@unimicro.no>
2023-11-22 23:40:17 +0100

    wip

---
+++ src/Plugin.svelte
@@ -0,0 +1,15 @@
+<svelte:options customElement="unimicro-plugin" />
+
+<script lang="ts">
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
+</script>
+
+<button on:click={increment}>
+    Count: {count}
+</button>
+
+<style>
+</style>
