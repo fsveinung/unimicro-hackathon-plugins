988aea268bc08a89de363334447c292d5bef820a
Sveinung Farestveit <sveinung@unimicro.no>
2023-11-24 16:53:13 +0100

    Update Plugin.svelte

--- src/Plugin.svelte
+++ src/Plugin.svelte
@@ -1,6 +1,6 @@
 <svelte:options
     customElement={{
-        tag: 'unimicro-plugin',
+        tag: 'sb1-loan',
         props: {
             api: { reflect: true, type: 'Object', attribute: 'api' },
         },
@@ -9,39 +9,47 @@
 
 <script lang="ts">
     import { onMount } from 'svelte';
-
     export let api: any; // TODO: types
-
-    let count: number = 0;
-    let user: any;
+    let company: any;
 
     onMount(() => {
         if (api) {
-            loadUserData();
+            loadCompany();
         }
     });
 
-    async function loadUserData() {
-        user = await api.http.get('/api/biz/users?action=current-session');
+    async function loadCompany() {
+        company = await api.http.get('/api/biz/companysettings/1?select=CompanyName');
     }
 
-    function increment() {
-        count += 1;
+    function nextStep() {
+        //count += 1;
     }
 </script>
 
-{#if user}
-    <h1>Hello {user.DisplayName || user.UserName || user.Email}</h1>
+{#if company}
+    <section class="card">
+        <h1>Lånesøknad for {company.CompanyName}</h1>
+        <h2>Lånebeløp</h2>
+        <input type="text" />
+
+    </section>
+    <footer>
+        <button on:click={nextStep}>Neste</button>
+        <button on:click={api.showAlert('Videresendt!')}> Tilbakemelding </button>
+    </footer>
 {/if}
-
-<button on:click={increment}>
-    Count: {count}
-</button>
-
-<button on:click={api.showAlert('Hello from plugin!')}> Show alert </button>
 
 <style>
     h1 {
         font-weight: 500;
+    }
+    .card {
+        min-height: 10rem;
+    }
+    footer {
+        display: flex;
+        margin-top: 1px solid var(--border-color);
+        padding: 1rem;
     }
 </style>
