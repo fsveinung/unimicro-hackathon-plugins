1ed237c95204cd93025e849506027d5c9a1bc4b6
Sveinung Farestveit <sveinung@unimicro.no>
2024-05-24 10:41:01 +0200

    Doubleclick-editing

--- vanilla/journal/lib/editable.js
+++ vanilla/journal/lib/editable.js
@@ -15,16 +15,21 @@ export class Editable {
     init(tableNode, colMap) {
         this._tableNode = tableNode;
         this._columns = colMap;
-        this._tableNode.addEventListener("click", evt => this.onCellClick(evt));
-        this._tableNode.addEventListener("keydown", evt => this.keyDown(evt));
-        this._tableNode.addEventListener("resize", evt => this.onResize(evt));
+        this._tableNode.addEventListener("click", evt => this.#onCellClick(evt));        
+        this._tableNode.addEventListener("dblclick", evt => this.#onCellDblClick(evt));
+        this._tableNode.addEventListener("keydown", evt => this.#keyDown(evt));
+        this._tableNode.addEventListener("resize", evt => this.#onResize(evt));
     }
 
     focus() {
-        this.onCellClick();
+        this.#onCellClick();
     }
 
-    onCellClick(event) {
+    #onCellDblClick(event) {
+        this.#onCellClick(event, true);
+    }
+
+    #onCellClick(event, startEdit) {
         let cell = this._current.cell;
 
         // Event trigger?
@@ -34,63 +39,66 @@ export class Editable {
         }
 
         if (!cell) {
-            cell = this.getCellAt(0 , 1);
+            cell = this.#getCellAt(0 , 1);
         }
 
         if (this._current.isEditing) {
-            this._current.editor.stopEdit(true, this.getCellPosition(cell) );
+            this._current.editor.stopEdit(true, this.#getCellPosition(cell) );
             return;
         }
 
-        this.focusCell(cell);
+        this.#focusCell(cell, startEdit);
 
     }
 
-    focusCell(cell) {
+    #focusCell(cell, startEdit) {
         if (!cell.getAttribute("tabindex")) {
-            cell.setAttribute("tabindex", this.calcCellIndex(cell));
+            cell.setAttribute("tabindex", this.#calcCellIndex(cell));
         }
         cell.focus();
         this._current.cell = cell;
+        if (startEdit) {
+            this.#openEditor();
+        }
     }
 
-    keyDown(evt) {
+    #keyDown(evt) {
         const cell = this._current.cell;
 
         const nav = TableNavigation.detectNavigation(cell, evt);
         if (nav) {
-            const target = this.getCellAt(nav.col, nav.row);
+            const target = this.#getCellAt(nav.col, nav.row);
             if (target) {
-                this.focusCell(target);
+                this.#focusCell(target);
             }
             evt.preventDefault();
             return;            
         }
 
-        if (this.isTyping(evt)) {
-            this.openEditor(evt.key);
+        if (this.#isTyping(evt)) {
+            this.#openEditor();
         }
 
     }
 
-    isTyping(evt) {
+    #isTyping(evt) {
         //console.log(`isTyping(key:${evt.key}, code:${evt.code}, keyCode:${evt.keyCode})`);
         if (!(evt && evt.code && evt.key)) return false;
         const isRegularKey = (evt.code === `Key${evt.key.toUpperCase()}`);
         if (isRegularKey) return true;
-        if (evt.key === "Tab") return false;
-        if (evt.key === "Shift") return false;
+        if (evt.key === "Tab" || evt.key === "Shift" || evt.key === "Control") 
+            return false;
         // todo: check for virtual keys to ignore
         return true; 
     }
 
-    openEditor(key) {
+    #openEditor() {
         //console.log("StartEdit code: " + key);
         if (!this._current.editor) {
             this._current.editor = new CellEditor();
             this._current.editor.create(this._tableNode);
-            this._current.editor.onKeyDown( e => this.handleEditEvents(e));
-            this._current.editor.onClose( e => this.handleEditClosing(e));
+            this._current.editor.onKeyDown( e => this.#handleEditEvents(e));
+            this._current.editor.onClose( e => this.#handleEditClosing(e));
         }
         const cell = this._current.cell;
         const text = cell.innerText ?? "";
@@ -98,7 +106,7 @@ export class Editable {
         this._current.editor.startEdit(text, cell);
     }
 
-    handleEditEvents(event) {
+    #handleEditEvents(event) {
         //console.log("handleEditEvents", event);
     }
 
@@ -106,7 +114,7 @@ export class Editable {
      * Handler for when editor is closing
      * @param {{cell: HTMLTableCellElement, text: string, commit: boolean, nav: CellPosition}} event 
      */
-    handleEditClosing(event) {  
+    #handleEditClosing(event) {  
         //console.log("handleEditClosing", event);
         this._current.isEditing = false;
         let cell = event.cell ?? this._current.cell;
@@ -114,15 +122,15 @@ export class Editable {
             cell.innerText = event.text;            
         }
         if (event.nav) {
-            const nextCell = this.getCellAt(event.nav.col, event.nav.row);
+            const nextCell = this.#getCellAt(event.nav.col, event.nav.row);
             if (nextCell) {
                 cell = nextCell;
             }
         }        
-        this.focusCell(cell);
+        this.#focusCell(cell);
     }
 
-    getCellAt(colIndex, rowIndex) {
+    #getCellAt(colIndex, rowIndex) {
         const table = this._tableNode;        
         if (!table) return;
         if (table.rows.length < rowIndex) return;
@@ -136,8 +144,8 @@ export class Editable {
         return row.cells[colIndex];
     }
 
-    calcCellIndex(cell) {
-        const pos = this.getCellPosition(cell);
+    #calcCellIndex(cell) {
+        const pos = this.#getCellPosition(cell);
         return ((pos.row * 50) + pos.col) + 1000;
     }
 
@@ -146,14 +154,14 @@ export class Editable {
      * @param {HTMLTableCellElement} cell 
      * @returns {{ row: number, col: number }}
      */
-    getCellPosition(cell) {
+    #getCellPosition(cell) {
         return {
             row: cell.parentElement.rowIndex,
             col: cell.cellIndex
         };
     }    
 
-    onResize() {
+    #onResize() {
 
     }
 
